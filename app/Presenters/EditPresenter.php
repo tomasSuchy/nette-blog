86e2f56013dfc0be80e5d8ce02f397fdcc4f02cd
Tomáš Suchý <tomas.suchy@vivantis.cz>
2024-04-03 17:37:15 +0200

    init

---
+++ app/Presenters/EditPresenter.php
@@ -0,0 +1,56 @@
+<?php
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+final class EditPresenter extends Nette\Application\UI\Presenter
+{
+	public function __construct(
+		private Nette\Database\Explorer $database,
+	) {
+	}
+
+  protected function createComponentPostForm(): Form {
+    $form = new Form;
+    $form->addText('title', 'Titulek:')
+      ->setRequired();
+    $form->addTextArea('content', 'Obsah:')
+      ->setRequired();
+
+    $form->addSubmit('send', 'Uložit a publikovat');
+    $form->onSuccess[] = [$this, 'postFormSucceeded'];
+
+    return $form;
+  }
+  public function postFormSucceeded(array $data): void {
+    $postId = $this->getParameter('postId');
+
+    if ($postId) {
+      $post = $this->database
+        ->table('posts')
+        ->get($postId);
+      $post->update($data);
+
+    } else {
+      $post = $this->database
+        ->table('posts')
+        ->insert($data);
+    }
+
+    $this->flashMessage('Příspěvek byl úspěšně publikován.', 'success');
+    $this->redirect('Post:show', $post->id);
+    }
+  public function renderEdit(int $postId): void {
+    $post = $this->database
+      ->table('posts')
+      ->get($postId);
+
+    if (!$post) {
+      $this->error('Post not found');
+    }
+
+    $this->getComponent('postForm')
+      ->setDefaults($post->toArray());
+  }
+}
---
+++ app/Presenters/HomePresenter.php
@@ -0,0 +1,20 @@
+<?php
+namespace App\Presenters;
+
+use Nette;
+use Nette\Application\UI\Form;
+
+
+final class HomePresenter extends Nette\Application\UI\Presenter
+{
+  public function __construct(
+    private Nette\Database\Explorer $database,
+  ) {
+
+  }
+  public function renderDefault(): void {
+    $this->template->posts = $this->database
+      ->table('posts')
+      ->order('created_at DESC');
+  }
+}
